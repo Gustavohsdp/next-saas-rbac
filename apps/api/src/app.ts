c9b2beb712b3cae5839a4d8064d261b76de9711a
Gustavohsdp <gustavohsdp@gmail.com>
2024-05-25 22:13:11 -0300

    feat: setup fastify

---
+++ apps/api/src/app.ts
@@ -0,0 +1,20 @@
+import cors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { createAccount } from './http/auth/create-account'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(createAccount)
