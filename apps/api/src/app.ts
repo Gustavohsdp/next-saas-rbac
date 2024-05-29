05bb0100f2b58d1b916a425e91a12cf8e2fe086e
Gustavohsdp <gustavohsdp@gmail.com>
2024-05-29 18:25:40 -0300

    feat: get invites

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -20,6 +20,7 @@ import { requestPasswordRecover } from './http/routes/auth/request-password-reco
 import { resetPassword } from './http/routes/auth/reset-password'
 import { createInvite } from './http/routes/invites/create-invite'
 import { getInvite } from './http/routes/invites/get-invite'
+import { getInvites } from './http/routes/invites/get-invites'
 import { getMembers } from './http/routes/members/get-member'
 import { removeMember } from './http/routes/members/remove-member'
 import { updateMembers } from './http/routes/members/update-member'
@@ -99,3 +100,4 @@ app.register(updateMembers)
 app.register(removeMember)
 app.register(createInvite)
 app.register(getInvite)
+app.register(getInvites)
