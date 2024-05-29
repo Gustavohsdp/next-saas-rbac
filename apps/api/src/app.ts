bb92b07b541ebcc458382e46a0737186731b8d7f
Gustavohsdp <gustavohsdp@gmail.com>
2024-05-29 18:52:04 -0300

    feat: get pending invites

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -22,6 +22,7 @@ import { acceptInvite } from './http/routes/invites/accept-invite'
 import { createInvite } from './http/routes/invites/create-invite'
 import { getInvite } from './http/routes/invites/get-invite'
 import { getInvites } from './http/routes/invites/get-invites'
+import { getPendingInvites } from './http/routes/invites/get-pending-invites'
 import { rejectInvite } from './http/routes/invites/reject-invite'
 import { revokeInvite } from './http/routes/invites/revoke-invite'
 import { getMembers } from './http/routes/members/get-member'
@@ -107,3 +108,4 @@ app.register(getInvites)
 app.register(acceptInvite)
 app.register(rejectInvite)
 app.register(revokeInvite)
+app.register(getPendingInvites)
