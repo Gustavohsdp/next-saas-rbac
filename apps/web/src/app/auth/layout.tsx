7c4186e4316b113f84a354b718537c8ad1c1b5f1
Gustavohsdp <gustavohsdp@gmail.com>
2024-09-20 20:45:58 -0300

    feat: header component

--- apps/web/src/app/auth/layout.tsx
+++ apps/web/src/app/auth/layout.tsx
@@ -1,11 +1,15 @@
+import { redirect } from 'next/navigation'
+
+import { isAuthenticated } from '@/auth/auth'
+
 export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  // if (isAuthenticated()) {
-  //   redirect('/')
-  // }
+  if (isAuthenticated()) {
+    redirect('/')
+  }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-4">
